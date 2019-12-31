92588760fbe499be87f3da4c0ec8e9c5cbfb010e
Abdullah Kamal <abdullahkamal@live.com>
2019-12-31 17:11:59 +0530

    Add msg from branch1

--- say_hello.bat
+++ say_hello.bat
@@ -1,1 +1,4 @@
 echo "Hello Jenkins from GitHub"
+echo "I am using feature branch1 from GitHub effective Jenkins"
+
+
--- say_hello.sh
+++ say_hello.sh
@@ -1,1 +1,2 @@
 echo "Hello Jenkins from GitHub"
+echo "I am using feature branch1 from GitHub effective Jenkins"
